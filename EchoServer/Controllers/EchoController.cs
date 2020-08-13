a58335d489c338aec0b712fabdb252a6b473e1e8
chenbojian <bjchen@thoughtworks.com>
2020-08-13 10:10:38 +0000

    [bojian] add code

---
+++ EchoServer/Controllers/EchoController.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace EchoServer.Controllers
+{
+    public class EchoController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public Dictionary<string, string> Index()
+        {
+            var headers = Request.Headers;
+            return Request.Headers.ToDictionary(h =>h.Key, h => string.Join(",", h.Value));
+        }
+    }
+}
---
+++ EchoServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EchoServer.WebApiApplication" Language="C#" %>
